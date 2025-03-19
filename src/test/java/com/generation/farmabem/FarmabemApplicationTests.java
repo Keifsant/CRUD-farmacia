9d2fa09844fa8573984a1f1faee95a97c3c2a874
KeifSant <keyllafsantos1@gmail.com>
2025-03-19 10:17:01 -0300

    First Commit

---
+++ src/test/java/com/generation/farmabem/FarmabemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.farmabem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmabemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
